028c9f507fc85243268bd73abba10e64af570053
Rysek <36196503+rezatera854@users.noreply.github.com>
2021-03-21 22:02:16 +0100

    CNF

--- test3.py
+++ test3.py
@@ -1,10 +1,11 @@
 import pysat
-from pysat.formula import WCNF
+from pysat.formula import CNF
 from pysat.examples.rc2 import RC2Stratified
-formula = WCNF(from_file='instance1.wcnf')
-
+formula = CNF(from_file='quinn.cnf')
+print(formula.clauses[1])
+"""
 rc2g4 = RC2Stratified(formula,solver="g4")
 rc2g4 = RC2Stratified(formula,solver="g4")
 model = rc2.compute()
 print("model = ",model)
-print("cost = ",rc2.cost)
+print("cost = ",rc2.cost)"""
